add328925bc3b289584ae5706992aed7185eef29
Smith Júnior <smith.junior@icloud.com>
2021-08-29 19:17:37 -0300

    Started the project

---
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,38 @@
+package  br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "{ \"pedido_id\": 4, \"preco\": 340.00 }";
+        var record  = new ProducerRecord<String, String>("LOJA_NOVO_PEDIDO", value, value);
+        producer.send(record, (data, ex) -> {
+            if(ex != null) {
+                ex.printStackTrace();
+            }
+            System.out.println(data.topic());
+        }).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getServerAddress());
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+
+    private static String getServerAddress(){
+        return  "192.168.1.110:9092";
+    }
+}
