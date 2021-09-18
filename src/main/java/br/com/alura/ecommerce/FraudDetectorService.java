798651ae0b2a0e7418a5df5b088a486bf49acf94
Smith Júnior <smith.junior@icloud.com>
2021-09-18 13:15:45 -0300

    Added the KafkaService to manage consumer topics

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,55 +1,32 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
-import java.util.UUID;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class FraudDetectorService {
     public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-        while(true){
-            var records = consumer.poll(Duration.ofMillis(500));
+        var fraudDetectorService = new FraudDetectorService();
+        var service = new KafkaService(
+                FraudDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudDetectorService::parse
+        );
+        service.run();
+    }
 
-            if(!records.isEmpty()){
-                System.out.println("Encontrei "+records.count() +" registros!");
-                for(var record: records) {
-                    System.out.println("----");
-                    System.out.println("Processando novo pedido, checkando por fraude!");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    System.out.println("----");
-                    try {
-                        Thread.sleep(5000);
-                    }catch(InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Pedido processado");
-                }
-            }
-
+    private void parse(ConsumerRecord<String, String> record){
+        System.out.println("----");
+        System.out.println("Processando novo pedido, checkando por fraude!");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        System.out.println("----");
+        try {
+            Thread.sleep(5000);
+        }catch(InterruptedException e) {
+            e.printStackTrace();
         }
-
+        System.out.println("Pedido processado");
     }
 
-    private static Properties properties(){
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, getServerAddress());
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getName()+"_"+ UUID.randomUUID().toString());
-        return properties;
-    }
-
-    private static String getServerAddress(){
-        return  "localhost:9092";
-    }
 }
---
+++ src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -0,0 +1,50 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+public class KafkaService {
+    private final KafkaConsumer<String, String> consumer;
+    private final ConsumerFunction parse;
+
+    public KafkaService(String groupIdName, String topic, ConsumerFunction parse){
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<String, String>(properties(groupIdName));
+        consumer.subscribe(Collections.singletonList(topic));
+
+    }
+
+    public void run(){
+        while(true){
+            var records = consumer.poll(Duration.ofMillis(500));
+
+            if(!records.isEmpty()){
+                System.out.println("Encontrei "+records.count() +" registros!");
+                for(var record: records) {
+                    this.parse.consume(record);
+                }
+            }
+        }
+    }
+
+    private static Properties properties(String groupIdName){
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, getServerAddress());
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupIdName);
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        return properties;
+    }
+
+    private static String getServerAddress(){
+        return  "localhost:9092";
+    }
+}
